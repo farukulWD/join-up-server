ef1cfc53370948e8acef8c4c0e6d155814ef2405
farukulWD <farukgb1999@gmail.com>
2025-06-30 20:54:11 +0600

    setup

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import app from "./app";
+import config from "./app/config";
+import connectDB from "./app/utils/db";
+
+async function bootstrap() {
+  try {
+    await connectDB();
+    app.listen(config.port, async () => {
+      console.log(`Server running on the ${config.port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+app.use("/", async (req, res) => {
+  res.send(`Server running in ${config.port}`);
+});
+bootstrap();
